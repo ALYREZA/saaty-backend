1487b4c0f7de932248e4bcc7b09a637157cd8f10
alireza <fanaticboy666@gmail.com>
2019-03-06 16:26:35 +0330

    create saat types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,11 @@ module Types
     field :all_users, [UserType], null: false
     field :all_clients, [ClientType], null: false
     field :all_projects, [ProjectType], null: false
+    field :all_saats, [SaatType], null: false
+
+    def all_saats
+      Saat.all
+    end
     def all_projects
       Project.all
     end
--- app/models/saat.rb
+++ app/models/saat.rb
@@ -1,4 +1,5 @@
 class Saat < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  belongs_to :client
 end

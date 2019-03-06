97c71643c83199fe9f0e01ae98eb66dee77b7d74
alireza <fanaticboy666@gmail.com>
2019-03-06 16:14:01 +0330

    create project types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,10 @@ module Types
   class QueryType < Types::BaseObject
     field :all_users, [UserType], null: false
     field :all_clients, [ClientType], null: false
+    field :all_projects, [ProjectType], null: false
+    def all_projects
+      Project.all
+    end
     def all_users
       User.all
     end

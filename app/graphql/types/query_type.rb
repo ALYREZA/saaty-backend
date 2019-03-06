26e466a2b3ef81237c69f4619b837f038f8c894e
alireza <fanaticboy666@gmail.com>
2019-03-06 16:03:24 +0330

    create client type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_users, [UserType], null: false
-
+    field :all_clients, [ClientType], null: false
     def all_users
       User.all
+    end
+    def all_clients
+      Client.all
     end
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3 }
+    belongs_to :user
 end

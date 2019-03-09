541f3051e0ed0fa6fcec2e1e45c310e31d51754a
alireza <fanaticboy666@gmail.com>
2019-03-09 17:17:21 +0330

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require 'bcrypt'
 class User < ApplicationRecord
     include BCrypt
-    after_validation :changePassword
+    after_validation :bcryptPassword
     validates :name, presence: true, length: { minimum: 3, maximun: 50 }
     validates :email, presence: true, uniqueness: true, email: true, length: { minimum: 6,maximun: 200 }
     validates :password, presence: true, length: { in: 6..20 }
@@ -9,7 +9,7 @@ class User < ApplicationRecord
         return Password.new(self.password) == pass
     end
     private 
-    def changePassword
+    def bcryptPassword
         self.password = Password.create(self.password)
     end
 end

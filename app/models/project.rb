f463938f329c5e3b782283fc4fcb9a84bf8cdaaa
alireza <fanaticboy666@gmail.com>
2019-03-15 13:48:23 +0330

    refacoring model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,5 @@ class Project < ApplicationRecord
   validates :color, presence: true,length: { is: 6 }, allow_blank: true
   belongs_to :user, counter_cache: :projects_count
   belongs_to :client, counter_cache: :projects_count
-  has_many :times, class_name: "Saat", foreign_key: "project_id"
+  has_many :times, class_name: "Saat", foreign_key: "project_id", dependent: :delete
 end

bbc34b0ae3b155a30d261b9a8e2e00c6efdb2df5
alireza <fanaticboy666@gmail.com>
2019-03-17 00:35:32 +0330

    complete delete for client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3, maximum: 100 },uniqueness: { scope: :user,
         message: "is the same for two client" }
-    belongs_to :user,counter_cache: :clients_count
-    has_many :projects, class_name: "Project", foreign_key: "client_id", dependent: :delete_all
+    belongs_to :user, counter_cache: :clients_count
+    has_many :projects, class_name: "Project", foreign_key: "client_id", :dependent => :destroy
     has_many :times, class_name: "Saat", foreign_key: "client_id"
 end
